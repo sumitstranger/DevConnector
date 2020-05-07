38bd666acf657825cb6e177788d3a1a99c54d666
sumitstranger <sumitrathore150598@gmail.com>
2020-05-07 11:39:48 +0530

    Add Like & dislike on Post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { POST_ERROR, GET_POSTS } from './types';
+import { POST_ERROR, GET_POSTS, UPDATE_LIKES } from './types';
 
 import { setAlert } from './alert';
 
@@ -11,6 +11,40 @@ export const getPosts = () => async (dispatch) => {
     dispatch({
       type: GET_POSTS,
       payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Add likes
+
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//remove likes
+
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
     });
   } catch (err) {
     dispatch({
